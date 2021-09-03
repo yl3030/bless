d204287d0b09088d6c7fb15bc39bd25017cf4aa1
yl301114 <yl301114@gmail.com>
2021-09-04 04:22:55 +0800

    電腦版完成

--- public/js/index.js
+++ public/js/index.js
@@ -13,15 +13,36 @@ $(".gototop").click(function(){
     $("html,body").animate({scrollTop:0},parseInt(300));
 })
 
+// 想、念、願
 $(".think").click(function(){
-    $(this).addClass("active");
-    $(".step-text").children(".original").hide();
+    $(".step-text").children("p").hide().removeClass("active");
+    // $(".step-text").children(".original").hide();
     $(".step-text").children(".step1").fadeIn(300).addClass("active");
+    $(".step").children(".text").removeClass("flash active");
+    $(this).addClass("active");
     $(".say").addClass("flash");
 })
 $(".say").click(function(){
-    $(this).addClass("active");
-    $(".step-text").children("p").hide();
+    $(".step-text").children("p").hide().removeClass("active");
     $(".step-text").children(".step2").fadeIn(300).addClass("active");
+    $(".step").children(".text").removeClass("flash active");
+    $(this).addClass("active");
     $(".will").addClass("flash");
+})
+$(".will").click(function(){
+    $(".step-text").children("p").hide().removeClass("active");
+    $(".step-text").children(".step3").fadeIn(300).addClass("active");
+    $(".step").children(".text").removeClass("flash active");
+    $(this).addClass("active");
+    // $(".will").addClass("flash");
+})
+
+// menu
+$(".menu-nav").children("li").click(function(){
+    $(this).toggleClass("active");
+    $(this).children(".sub-menu").slideToggle(300);
+})
+$(".menu-icon").click(function(){
+    $(this).toggleClass("active");
+    $(".main-menu").toggleClass("active");
 })
